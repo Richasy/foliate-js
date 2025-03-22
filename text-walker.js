5e30392ffe2ff9486465e29aa91ff4e954640df6
Richasy <Thansy@foxmail.com>
2025-03-22 16:14:33 +0800

    Update text-walker.js

--- text-walker.js
+++ text-walker.js
@@ -21,7 +21,7 @@ const filter = NodeFilter.SHOW_ELEMENT | NodeFilter.SHOW_TEXT
 const acceptNode = node => {
     if (node.nodeType === 1) {
         const name = node.tagName.toLowerCase()
-        if (name === 'script' || name === 'style') return NodeFilter.FILTER_REJECT
+        if (name === 'script' || name === 'style' || name === 'h1') return NodeFilter.FILTER_REJECT
         return NodeFilter.FILTER_SKIP
     }
     return NodeFilter.FILTER_ACCEPT
@@ -32,7 +32,7 @@ export const textWalker = function* (x, func) {
     const walker = document.createTreeWalker(root, filter, { acceptNode })
     const walk = x.commonAncestorContainer ? walkRange : walkDocument
     const nodes = walk(x, walker)
-    const strs = nodes.map(node => node.nodeValue)
+    const strs = nodes.map(node => { return node.nodeValue })
     const makeRange = (startIndex, startOffset, endIndex, endOffset) => {
         const range = document.createRange()
         range.setStart(nodes[startIndex], startOffset)
