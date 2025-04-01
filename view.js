be4eaec2b960684dc80e8ef789e610ef378bc94b
Richasy <Thansy@foxmail.com>
2025-04-01 22:16:07 +0800

    Update view.js

--- view.js
+++ view.js
@@ -302,7 +302,6 @@ export class View extends HTMLElement {
     this.renderer.addEventListener("create-overlayer", (e) =>
       e.detail.attach(this.#createOverlayer(e.detail))
     );
-    console.log(book);
     this.renderer.open(book);
     this.#root.append(this.renderer);
 
